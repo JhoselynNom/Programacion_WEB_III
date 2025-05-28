8cb1e7b8c2cfc35a4e9ad179a5fd7f19c14d07a5
Jhoselyn Noemi Ajhuacho Tarquino <jhoselynat26@gmail.com>
2025-05-27 20:22:24 -0400

    Mensaje descriptivo del cambio

---
+++ backend/model/ProductModel.js
@@ -0,0 +1,21 @@
+const db = require('../config/db');
+
+const ProductModel = {
+    getAll: (callback) => {
+        db.query('SELECT * FROM productos', callback);
+    },
+    getById: (id, callback) => {
+        db.query('SELECT * FROM productos WHERE id = ?', [id], callback);
+    },
+    create: (data, callback) => {
+        db.query('INSERT INTO productos SET ?', [data], callback);
+    },
+    update: (id, data, callback) => {
+        db.query('UPDATE productos SET ? WHERE id = ?', [data, id], callback);
+    },
+    delete: (id, callback) => {
+        db.query('DELETE FROM productos WHERE id = ?', [id], callback);
+    }
+};
+
+module.exports = ProductModel;
